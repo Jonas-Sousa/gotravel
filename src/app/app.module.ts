48247f0f69feb113890511b31cddb371ae4a262e
Jonas sousa <jonassousagtr@gmail.com>
2023-05-11 21:25:28 -0300

    Feat: Criando o cabeçalho e navbar do site e suas estilizações.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componente/navbar/navbar.component';
+import { HeaderComponent } from './componente/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
