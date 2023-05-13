23d511f423935728b1fa17cfd730d57590449759
Jonas sousa <jonassousagtr@gmail.com>
2023-05-12 21:56:55 -0300

    Refactor: Instalando o tailwind css e refaotando o css todo do site.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {},
+  fontSize: {
+    sm: '1rem',
+    md: '2rem',
+    xl: '4rem',
+    '2xl': '1.563rem',
+    '3xl': '1.953rem',
+    '4xl': '2.441rem',
+    '5xl': '3.052rem',
+  }
+};
+export const plugins = [];
+
