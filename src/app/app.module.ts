92db59e4386b523b8c5df582dc289223426f4e80
Jonas sousa <jonassousagtr@gmail.com>
2023-05-13 11:59:37 -0300

    Feat: Criando os cards para o carrossel.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './componente/navbar/navbar.component';
-import { HeaderComponent } from './componente/header/header.component';
+import { NavbarComponent } from './componente/Navbar/navbar.component';
+import { HeaderComponent } from './componente/Header/header.component';
+import { PopularesComponent } from './componente/populares/populares.component';
+import { CardsComponent } from './componente/populares/Cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    PopularesComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
