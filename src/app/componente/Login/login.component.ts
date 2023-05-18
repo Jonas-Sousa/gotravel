9fa79cd74d93ed86e48c239623ff63b0fb093fe0
Jonas sousa <jonassousagtr@gmail.com>
2023-05-18 19:45:11 -0300

    Feat: Criando algumas validações nos inputs e finalizando os layout da pag de login.

--- src/app/componente/Login/login.component.ts
+++ src/app/componente/Login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -18,17 +18,36 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.formLogin = this.formBuilder.group({
-      email: [''],
-      password: ['']
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+
+      ])],
+      password: ['', Validators.required
+      ]
     })
   }
 
   salvarDados() {
-    const jsonData = {
-      dadosLogin: this.formLogin.value,
-    };
-    console.log(jsonData);
+    console.log(this.formLogin.get('email')?.errors);
+    if(this.formLogin.valid == true){
+      const jsonData = {
+        dadosLogin: this.formLogin.value,
+      };
+      console.log(jsonData);
+    }
   }
+
+  isPasswordInvalid() {
+    const passwordControl = this.formLogin.get('password');
+    return passwordControl?.invalid && passwordControl?.touched;
+  }
+
+  isEmailInvalid() {
+    const emailControl = this.formLogin.get('email');
+    return emailControl?.invalid && emailControl?.touched;
+  }
+
 
 
 
