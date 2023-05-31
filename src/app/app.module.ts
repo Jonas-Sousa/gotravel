e7eccacb1ffafe2e2713841c484f15d85c257c5e
Jonas sousa <jonassousagtr@gmail.com>
2023-05-31 06:39:54 -0300

    Reafatorando componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,30 +8,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componente/Header/header.component';
 import { PopularesComponent } from './componente/populares/populares.component';
 import { CardsComponent } from './componente/populares/Cards/cards.component';
-import { ContatoComponent } from './componente/Contato/contato.component';
 import { JornadaComponent } from './componente/Jornada/jornada.component';
 import { FotosCardsComponent } from './componente/Jornada/fotos-cards/fotos-cards.component';
 import { LoginComponent } from './componente/Login/login.component';
 import { PagPrincipalComponent } from './componente/pag-principal/pag-principal.component';
 import { CadastrarComponent } from './componente/Cadastrar/cadastrar.component';
 import { NavComponent } from './componente/nav/nav.component';
+import { HeadComponent } from './componente/head/head.component';
+import { VideoComponent } from './componente/video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     PopularesComponent,
     CardsComponent,
-    ContatoComponent,
     JornadaComponent,
     FotosCardsComponent,
     LoginComponent,
     PagPrincipalComponent,
     CadastrarComponent,
     NavComponent,
+    HeadComponent,
+    VideoComponent,
   ],
   imports: [
     BrowserModule,
